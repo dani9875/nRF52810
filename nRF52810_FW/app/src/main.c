1bce8fab9fab78a02f2962d7be8bc0df35a1c5ff
dani9875 <dan.hatar94@gmail.com>
2024-07-24 15:13:43 +0200

    First usable build with a custom board from sample example project

--- nRF52810_FW/app/src/main.c
+++ nRF52810_FW/app/src/main.c
@@ -4,22 +4,20 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
-
 int main(void)
 {
+
 	printk("Use the sensor to change LED blinking period\n");
 
-	while (1) 
-	{
+	while (1) {
+
 		k_sleep(K_MSEC(100));
 	}
 
 	return 0;
 }
+
