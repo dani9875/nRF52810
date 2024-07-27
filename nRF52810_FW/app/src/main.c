570f53132e9c69fb4436cd2df33e135b575fbdc7
dani9875 <dan.hatar94@gmail.com>
2024-07-27 11:52:04 +0200

    Working blinking example

--- nRF52810_FW/app/src/main.c
+++ nRF52810_FW/app/src/main.c
@@ -4,20 +4,41 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
-int main(void)
+#define LED_NODE DT_ALIAS(led0)
+#if !DT_NODE_HAS_STATUS(LED_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+void main(void)
 {
+    int ret;
 
-	printk("Use the sensor to change LED blinking period\n");
+    if (!device_is_ready(led.port)) {
+        LOG_ERR("LED device not ready");
+        return;
+    }
 
-	while (1) {
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        LOG_ERR("Failed to configure LED pin");
+        return;
+    }
 
-		k_sleep(K_MSEC(100));
-	}
+    printk("Blinking LED on P0.20\n");
 
-	return 0;
+    while (1) 
+	{
+        gpio_pin_toggle_dt(&led);
+        k_sleep(K_MSEC(1000));
+    }
+
+	return;
 }
-
